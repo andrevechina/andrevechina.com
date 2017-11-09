c54e88f4f41cf34f7f1395602c0b1dca32a3118c
André Vechina <andre.vechina@blip.pt>
2017-11-09 13:16:13 +0000

    add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-(() => {
-    console.log('Hello World!')
-})()
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    React.createElement('h1', null, 'Hello world!'),
+    document.getElementById('app')
+);
