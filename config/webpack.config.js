a0b980d24605ac536476578f921c9798b2352f80
André Vechina <andre.vechina@blip.pt>
2017-11-09 14:27:05 +0000

    fix resolve extensions

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['js', 'jsx']
+        extensions: ['.js', '.jsx']
     },
     module: {
         rules: [{
